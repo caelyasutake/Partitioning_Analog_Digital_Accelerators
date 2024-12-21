e24705f305a6bc0a328c511b46c29dcd975e1921
Seojin Yoon <51718924+syoon2021@users.noreply.github.com>
2024-12-20 22:31:15 -0500

    Update utils.py

--- utils.py
+++ utils.py
@@ -24,44 +24,49 @@ from aihwkit.inference import PCMLikeNoiseModel, GlobalDriftCompensation
 from aihwkit.simulator.parameters import IOParameters
 
 def create_rpu_config(g_max=25, tile_size=512, modifier_std=0.07):
-  rpu_config = InferenceRPUConfig()
-  rpu_config.mapping.digital_bias = True
-  rpu_config.mapping.weight_scaling_omega = 1.0
-  rpu_config.mapping.weight_scaling_columnwise = True
-  rpu_config.mapping.learn_out_scaling = True
-  rpu_config.mapping.out_scaling_columnwise = True
-  rpu_config.mapping.max_input_size = tile_size
-  rpu_config.mapping.max_output_size = tile_size
-  rpu_config.noise_model = PCMLikeNoiseModel(g_max=g_max)
-  rpu_config.remap.type = WeightRemapType.CHANNELWISE_SYMMETRIC
-  rpu_config.clip.type = WeightClipType.FIXED_VALUE
-  rpu_config.clip.fixed_value = 1.0
-  rpu_config.modifier.type = WeightModifierType.MULT_NORMAL
-  rpu_config.modifier.rel_to_actual_wmax = True
-  rpu_config.modifier.std_dev = modifier_std
-  rpu_config.forward = IOParameters()
-  rpu_config.forward.out_noise = 0.05
-  rpu_config.forward.inp_res = 1 / (2 ** 8 - 2)
-  rpu_config.forward.out_res = 1 / (2 ** 8 - 2)
-  rpu_config.drift_compensation = GlobalDriftCompensation()
-  return rpu_config
+    rpu_config = InferenceRPUConfig()
+    rpu_config.mapping.digital_bias = True
+    rpu_config.mapping.weight_scaling_omega = 1.0
+    rpu_config.mapping.weight_scaling_columnwise = True
+    rpu_config.mapping.learn_out_scaling = True
+    rpu_config.mapping.out_scaling_columnwise = True
+    rpu_config.mapping.max_input_size = tile_size
+    rpu_config.mapping.max_output_size = tile_size
+    rpu_config.noise_model = PCMLikeNoiseModel(g_max=g_max)
+    rpu_config.remap.type = WeightRemapType.CHANNELWISE_SYMMETRIC
+    rpu_config.clip.type = WeightClipType.FIXED_VALUE
+    rpu_config.clip.fixed_value = 1.0
+    rpu_config.modifier.type = WeightModifierType.MULT_NORMAL
+    rpu_config.modifier.rel_to_actual_wmax = True
+    rpu_config.modifier.std_dev = modifier_std
+    rpu_config.forward = IOParameters()
+    rpu_config.forward.out_noise = 0.05
+    rpu_config.forward.inp_res = 1 / (2 ** 8 - 2)
+    rpu_config.forward.out_res = 1 / (2 ** 8 - 2)
+    rpu_config.drift_compensation = GlobalDriftCompensation()
+    return rpu_config
 
 def compute_hardware_metrics(model, outputs):
-  rpu_config = model.rpu_config
-  temperature = np.random.uniform(0.8, 1.2) * rpu_config.forward.inp_res * rpu_config.forward.out_res
-  noise = np.random.normal(0, rpu_config.forward.out_noise)
-  drift = rpu_config.drift_compensation.readout(outputs)
-  return temperature, noise, drift
+    rpu_config = model.rpu_config
+    temperature = np.random.uniform(0.8, 1.2) * rpu_config.forward.inp_res * rpu_config.forward.out_res
+    noise = np.random.normal(0, rpu_config.forward.out_noise)
+    drift = rpu_config.drift_compensation.readout(outputs)
+    return temperature, noise, drift
 
 def compute_hardware_loss(model, outputs):
-  temperature, noise, drift = compute_hardware_metrics(model, outputs)
-  hardware_loss = 0.2 * temperature + 0.3 * noise + 0.2 * drift
-  return hardware_loss
+    temperature, noise, drift = compute_hardware_metrics(model, outputs)
+    hardware_loss = 0.2 * temperature + 0.3 * noise + 0.2 * drift
+    return hardware_loss
 
 def update_rpu_config(rpu_config, perturbation_scale=0.01):
-  rpu_config.forward.inp_res += rpu_config.forward.inp_res * perturbation_scale
-  rpu_config.forward.out_res += rpu_config.forward.inp_res * perturbation_scale
-  rpu_config.forward.out_noise += rpu_config.forward.inp_res * perturbation_scale
-  g_max = rpu_config.noise_model.g_max + rpu_config.noise_model.g_max * perturbation_scale
-  rpu_config.noise_model = PCMLikeNoiseModel(g_max)
-  rpu_config.modifier.std_dev += rpu_config.modifier.std_dev * perturbation_scale
+    rpu_config.forward.inp_res += rpu_config.forward.inp_res * perturbation_scale
+    rpu_config.forward.out_res += rpu_config.forward.inp_res * perturbation_scale
+    rpu_config.forward.out_noise += rpu_config.forward.inp_res * perturbation_scale
+    g_max = rpu_config.noise_model.g_max + rpu_config.noise_model.g_max * perturbation_scale
+    rpu_config.noise_model = PCMLikeNoiseModel(g_max)
+    rpu_config.modifier.std_dev += rpu_config.modifier.std_dev * perturbation_scale
+
+def create_sgd_optimizer(model, learning_rate):
+    optimizer = AnalogSGD(model.parameters(), lr=learning_rate)
+    optimizer.regroup_param_groups(model)
+    return optimizer
